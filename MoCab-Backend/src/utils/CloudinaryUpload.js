3c25356b99ed6eeefe80ed765e803b80af904652
sankarshan <ssankarshan636@gmail.com>
2025-04-17 17:40:50 +0530

    Add Cloudinary upload function and Multer middleware for file handling

--- MoCab-Backend/src/utils/CloudinaryUpload.js
+++ MoCab-Backend/src/utils/CloudinaryUpload.js
@@ -8,3 +8,27 @@ cloudinary.config({
   api_secret:CLOUDINARY_API_SECRET
 })
 // upload images and video from local file (public/temp) to cloudinary 
+
+const uploadOnCloudinary = async(localFilePath) => {
+    try {
+        if (!localFilePath) {
+            return null;
+        }
+        const response =await cloudinary.uploader.upload(localFilePath,{
+            folder:"MoCab_folder",resource_type:"auto"
+        })
+        console.log("File uploaded successfully on cloudinary",response.url);
+        // After upload of file delete the file form local path
+        if(fs.existsSync(localFilePath)){
+            fs.unlinkSync(localFilePath)
+        }        
+    } catch (error) {
+        console.log("Error in uploading the file", error);
+        if(fs.existsSync(localFilePath)){
+            fs.unlinkSync(localFilePath);
+        }
+        return null;
+    }
+}
+
+export { uploadOnCloudinary }
